aa51c05ffb864e8ba032a7b9525fa15985c50748
Blake Severson <tarsonis21@gmail.com>
2025-11-13 22:52:53 -0800

    feat: Just add og title and description

--- resources/js/Pages/Item/ItemView.vue
+++ resources/js/Pages/Item/ItemView.vue
@@ -1,6 +1,6 @@
 <script setup>
 import html2canvas from "html2canvas";
-import {onMounted, ref} from "vue";
+import { onMounted, ref } from "vue";
 
 import SimpleBreadcrumb from "@/Components/SimpleBreadcrumb.vue";
 import Tipper from "@/Components/Tipper.vue";
@@ -18,8 +18,8 @@ const props = defineProps({
 });
 
 const breadcrumbs = ref([
-    {label: "Items", url: route("item.index")},
-    {label: props.item.name},
+    { label: "Items", url: route("item.index") },
+    { label: props.item.name },
 ]);
 
 const tooltipContainer = ref(null);
@@ -28,20 +28,31 @@ onMounted(() => {
     invisibleDiv.innerHTML = tooltipContainer.value.innerHTML;
     document.body.appendChild(invisibleDiv);
 
-    html2canvas(invisibleDiv, {width: 400, backgroundColor: null}).then((canvas) => {
-        ogBase64.value = canvas.toDataURL("image/png");
+    html2canvas(invisibleDiv, { width: 400, backgroundColor: null }).then(
+        (canvas) => {
+            ogBase64.value = canvas.toDataURL("image/png");
 
-        document.body.removeChild(invisibleDiv);
-    });
+            document.body.removeChild(invisibleDiv);
+        },
+    );
 });
 </script>
 
 <template>
     <Head>
         <title>{{ item.name }}</title>
-        <meta property="og:image" :content="route('decode.img', { q: JSON.stringify(ogBase64) })"/>
+        <meta
+            property="og:image"
+            :content="route('decode.img', { q: JSON.stringify(ogBase64) })"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" :content="item.name" />
+        <meta
+            property="og:description"
+            :content="[item.type, item.sub_type.item.slot].filter().join(' - ')"
+        />
     </Head>
-    <SimpleBreadcrumb :data="breadcrumbs"/>
+    <SimpleBreadcrumb :data="breadcrumbs" />
 
     <div class="px-6 lg:px-0 py-12">
         <div ref="tooltipContainer" class="flex items-center justify-center">
